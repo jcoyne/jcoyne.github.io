70ea4a1c66bc282231dc42faf5a80b8853268cf7
Justin Coyne <jcoyne@justincoyne.com>
2026-09-21 16:12:25 -0500

    Update auranav

--- auranav/service-worker.js
+++ auranav/service-worker.js
@@ -1,7 +1,7 @@
-// The build replaces 9b60a8d39922b399 with a hash of the deployed files. A new deployment
+// The build replaces 0995cd49ec80466f with a hash of the deployed files. A new deployment
 // therefore changes this script, which is what makes the browser reinstall the worker
 // and recache the shell; a byte-identical script is never reinstalled.
-const SHELL_CACHE = "chartplotter-shell-9b60a8d39922b399";
+const SHELL_CACHE = "chartplotter-shell-0995cd49ec80466f";
 const BASE = new URL("./", self.registration.scope);
 const STATIC_ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./app-icon-192.png", "./app-icon-512.png", "./fonts/Noto%20Sans%20Regular/0-255.pbf", "./fonts/OFL.txt", "./fonts/README.txt"];
 
